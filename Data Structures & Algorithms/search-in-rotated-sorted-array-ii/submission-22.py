4c4bc8b2e096c0044550f4c0a639abd76735a17a
MadApe <madapebanjo@gmail.com>
2026-04-19 13:58:40 -0500

    Add: search-in-rotated-sorted-array-ii - submission-22

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-22.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        l = 0
+        r = len(nums) - 1
+        mid = (r - l) // 2
+
+        while l <= r:
+            if nums[mid] == target:
+                return True
+
+            # identify which side of the list is sorted
+            if nums[l] < nums[mid]:
+                # left side is sorted
+                if target >= nums[l] and target < nums[mid]:
+                    # the target must be on the sorted side -> look left
+                    r = mid - 1
+                else:
+                    # the target is not on the sorted side -> look right
+                    l = mid + 1
+            elif nums[l] > nums[mid]:
+                # right side is sorted
+                if target > nums[mid] and target <= nums[r]:
+                    # the target is on the sorted side -> look right
+                    l = mid + 1
+                else:
+                    # the target is not on the sorted side -> look left
+                    r = mid - 1
+            else:
+                # must be duplicates so increment l until we know which side is sorted
+                l += 1               
+            
+            mid = ((r - l) // 2) + l
+        
+        return False
+

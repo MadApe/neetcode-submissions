33c89e145039ecbe4c9dd973c9fed1a8e2288730
MadApe <madapebanjo@gmail.com>
2026-04-19 14:31:31 -0500

    Add: reverse-integer - submission-21

---
+++ Data Structures & Algorithms/reverse-integer/submission-21.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # brute force
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        x_str = str(x)
+        x_str.lstrip('-')
+        new_str = ""
+
+        for i in range(len(x_str)):
+            new_str += x_str[-i-1]
+
+        v = int(new_str)
+        v = v * sign
+
+        if v < -2**31 or v > 2**31:
+            return 0
+
+        return v

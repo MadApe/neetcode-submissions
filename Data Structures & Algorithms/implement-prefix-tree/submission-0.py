7355266c46a133ac462a7a80cc5585b3a190125b
MadApe <madapebanjo@gmail.com>
2026-04-19 20:35:59 -0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,39 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.is_full_word = False
+
+    def insert(self, word: str) -> None:
+        node = self
+
+        for c in word:
+            if c not in node.children:
+                node.children[c] = PrefixTree()
+
+            node = node.children[c]
+        
+        node.is_full_word = True
+
+
+    def search(self, word: str) -> bool:
+        node = self
+        for c in word:
+            if c not in node.children:
+                return False
+            
+            node = node.children[c]
+        
+        return node.is_full_word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self
+        for c in prefix:
+            if c not in node.children:
+                return False
+            
+            node = node.children[c]
+        
+        return True
+        

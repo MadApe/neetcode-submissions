2783fb2515ca0648951289b98fcf0f51c95d5e3e
MadApe <madapebanjo@gmail.com>
2026-04-29 19:04:25 -0500

    Add: valid-sudoku - submission-14

---
+++ Data Structures & Algorithms/valid-sudoku/submission-14.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        size = len(board)
+        s_size = size // 3
+
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+        columns = defaultdict(set)
+        
+        for row in range(size):
+            for col in range(size):
+                square = (row // s_size) * s_size + col // s_size
+                if board[row][col] == '.':
+                    continue
+                
+                n = int(board[row][col])
+                if n in squares[square] or n in rows[row] or n in columns[col]:
+                    return False
+
+                squares[square].add(n)
+                rows[row].add(n)
+                columns[col].add(n)
+
+        return True
+
